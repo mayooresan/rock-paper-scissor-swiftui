231a2897ab1a88882ce900ef1c7cc75075f7aa5a
Jeya Mayoo <mayu3g@gmail.com>
2026-05-20 21:46:19 +0800

    Initial Commit

---
+++ Rock Paper Scissor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissor
+//
+//  Created by Jeya Mayoo on 20/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock Paper Scissor/Rock_Paper_ScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorApp.swift
+//  Rock Paper Scissor
+//
+//  Created by Jeya Mayoo on 20/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
