03b55264c1e18d075b0e234bd30dc23f8e9c0baa
Jeya Mayoo <mayu3g@gmail.com>
2026-06-09 02:41:36 +0800

    stats bug fixed

---
+++ Rock Paper Scissor/views/StatsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsView.swift
+//  Rock Paper Scissor
+//
+//  Created by Jeya Mayoo on 9/6/26.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
